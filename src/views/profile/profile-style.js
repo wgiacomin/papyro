40f43f95564f4ddbed8a8c817cc70076066dad49
Ana <anacaroldolata@gmail.com>
2022-02-26 17:46:09 -0300

    formatação dos componentes

--- src/views/profile/profile-style.js
+++ src/views/profile/profile-style.js
@@ -15,15 +15,26 @@ export default StyleSheet.create({
     fontSize: 32,
     lineHeight: 48,
     textAlign: 'center',
+    right: '6%',
     color: '#181D2D',
   },
   normal:{
     fontFamily: 'Poppins',
     fontStyle: 'normal',
     fontWeight: 'normal',
-    fontSize: 14,
-    lineHeight: 27,
+    fontSize: 12,
+    lineHeight: 23,
     textAlign: 'left',
+    color: '#000000',
+    marginLeft: 73
+  },
+  infos:{
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 14,
+    lineHeight: 17,
+    left: 180,
     color: '#000000',
   },
   bold:{
@@ -32,10 +43,51 @@ export default StyleSheet.create({
     fontWeight: 'normal',
     fontSize: 22,
     lineHeight: 33,
-    textAlign: 'center',
+    textAlign: 'left',
     color: '#181D2D',
-    marginLeft: 10
+    marginLeft: 26
   },
+  seeMore:{
+    fontFamily: 'DM Sans',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 13,
+    lineHeight: 22,
+    textAlign: 'left',
+    color: '#007AFF',
+    marginLeft: 26
+  },
+  buttonEdit: {
+    left: 320,
+    width: 20,
+    height: 20,
+    flexDirection: 'row',
+  },
+  buttonMsg: {
+    left: 350,
+    width: 25,
+    height: 25,
+    flexDirection: 'row'
+  },
+  imageSize: {
+    width: 22,
+    height: 22,
+    left: 150,
+    flexDirection: 'row',
+    top: 18
+  },
+  profileSize: {
+    alignSelf: 'center',
+    borderRadius: 16,
+    width: 128,
+    height: 118,
+  },
+  bookSize: {
+    alignSelf: 'flex-start',
+    width: 41,
+    height: 60,
+    marginLeft: 26
+  }
   
 
 })
--- src/views/profile/user-profile.js
+++ src/views/profile/user-profile.js
@@ -6,29 +6,38 @@ import vertical from '../../../assets/lines/straight.png'
 import profile from '../../../assets/icons/profileImage.png'
 import followers from '../../../assets/icons/followers.png'
 import booksReaded from '../../../assets/icons/booksReaded.png'
+import book from '../../../assets/icons/book.png'
 import BackButton from '../../components/back-button'
-import EditButton from '../../components/editProfile-button'
+import editButton from '../../../assets/buttons/editButton.png'
+import msgButton from '../../../assets/buttons/messagesButton.png'
 
 const UserProfile = ({ navigation }) => {
   return (
     <SafeAreaView style={safeView.AndroidSafeArea}>
       <BackButton navigation={navigation} />
+      <TouchableOpacity
+        onPress={() => navigation.navigate('GenreSelection')}>
+        <Image source={editButton} style={styles.buttonEdit} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('GenreSelection')}>
+        <Image source={msgButton} style={styles.buttonMsg} />
+      </TouchableOpacity>
       <View style={styles.standard}>
         <View style={styles.segment}>
           <Text style={styles.title}>
               Meu Perfil
           </Text>
-          <EditButton navigation={navigation}/>
           <Image source={vertical} style={styles.verticalLine} />
         </View>
         <View style={styles.segment}>
-          <Image source={profile} />
-          <Image source={followers} />
-          <Text style={styles.normal}>
+          <Image source={profile} style={styles.profileSize} />
+          <Image source={followers} style={styles.imageSize} />
+          <Text style={styles.infos}>
               Seguidores
           </Text>
-          <Image source={booksReaded} />
-          <Text style={styles.normal}>
+          <Image source={booksReaded} style={styles.imageSize}/>
+          <Text style={styles.infos}>
               Livros Lidos
           </Text>
           <Text style={styles.normal}>
@@ -40,61 +49,73 @@ const UserProfile = ({ navigation }) => {
           <Text style={styles.bold}>
               Grupos
           </Text>
-          <Image source={booksReaded} />
+          <Image source={book} style={styles.bookSize}/>
           <Text style={styles.normal}>
               nome Grupo
           </Text>
           <Text style={styles.normal}>
               X membros
           </Text>
-          <Text style={styles.normal}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('GenreSelection')}>
+            <Text style={styles.seeMore}>
               Ver mais(X)
-          </Text>
+            </Text>
+          </TouchableOpacity>  
         </View>
         <View style={styles.segment}>
           <Text style={styles.bold}>
               Lendo
           </Text>
-          <Image source={booksReaded} />
+          <Image source={book} style={styles.bookSize}/>
           <Text style={styles.normal}>
               Título
           </Text>
           <Text style={styles.normal}>
               Autor
           </Text>
-          <Text style={styles.normal}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('GenreSelection')}>
+            <Text style={styles.seeMore}>
               Ver mais(X)
-          </Text>
+            </Text>
+          </TouchableOpacity>  
         </View>
         <View style={styles.segment}>
           <Text style={styles.bold}>
               Lerei
           </Text>
-          <Image source={booksReaded} />
+          <Image source={book} style={styles.bookSize}/>
           <Text style={styles.normal}>
               Título
           </Text>
           <Text style={styles.normal}>
               Autor
           </Text>
-          <Text style={styles.normal}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('GenreSelection')}>
+            <Text style={styles.seeMore}>
               Ver mais(X)
-          </Text>
+            </Text>
+          </TouchableOpacity>  
         </View>
         <View style={styles.segment}>
           <Text style={styles.bold}>
               Lidos
           </Text>
-          <Image source={booksReaded} />
+          <Image source={book} style={styles.bookSize}/>
           <Text style={styles.normal}>
               Título
           </Text>
           <Text style={styles.normal}>
               Autor
           </Text>
-          <Text style={styles.normal}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('GenreSelection')}>
+            <Text style={styles.seeMore}>
               Ver mais(X)
-          </Text>
+            </Text>
+          </TouchableOpacity>  
         </View>
       </View>
     </SafeAreaView>
