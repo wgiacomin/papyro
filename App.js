f8109cb696a1a934af50cebb7c003a1933792962
Ana <anacaroldolata@gmail.com>
2022-03-05 16:44:21 -0300

    ajuste app.js

--- App.js
+++ App.js
@@ -1,17 +1,8 @@
 import * as Font from 'expo-font'
 import AppLoading from 'expo-app-loading'
 import React, { useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import welcomeScreen from './src/views/welcome/welcome'
-import loginScreen from './src/views/login/login'
-import forgetPassword from './src/views/login/forget-password'
-import register from './src/views/sign-up/register'
-import genreSelection from './src/views/genre-selection/genre-selection'
-import genreSelectionSec from './src/views/genre-selection/genre-selection-sec'
-import userProfile from './src/views/profile/user-profile'
-
-const Stack = createStackNavigator()
+import { AuthProvider } from './src/context/auth-context'
+import AppNavigator from './src/navigation/app-navigator'
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -19,7 +10,9 @@ const fetchFonts = () => {
     'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
     'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
     'Jura': require('./assets/fonts/Jura-Regular.ttf'),
-    'DM-Sans' : require('./assets/fonts/DMSans-Regular.ttf')
+    'DM-Sans' : require('./assets/fonts/DMSans-Regular.ttf'),
+    'Roboto': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
   })
 }
 
@@ -35,16 +28,8 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={welcomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name='Login' component={loginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name='ForgetPassword' component={forgetPassword} options={{ headerShown: false }} />
-        <Stack.Screen name='Register' component={register} options={{ headerShown: false}} />
-        <Stack.Screen name='GenreSelection' component={genreSelection} options={{ headerShown: false}} />
-        <Stack.Screen name='GenreSelection2' component={genreSelectionSec} options={{ headerShown: false}} />
-        <Stack.Screen name='UserProfile' component={userProfile} options={{ headerShown: false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <AppNavigator />
+    </AuthProvider>
   )
 }
