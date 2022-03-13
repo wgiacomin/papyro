a41d9d8f3aa47681cbead077750ae63ee048819b
AnaDolata <86605951+AnaDolata@users.noreply.github.com>
2022-03-13 13:14:53 -0300

    Front 37 tela de visualizar livros (#17)
    * criação dos elementos e posicionamento
    
    * formatação dos componentes
    
    * tela de perfil de usuário - sem barra de navegação
    
    * ajuste de nomeação
    
    * ajuste de arquivos
    
    * ajuste app.js
    
    * ajuste do botão de seleção de gênero
    
    * perfil do amigo
    
    * tela de notificação
    
    * tela de livros para ler
    
    * tela de livros lidos e que serão lidos
    
    Co-authored-by: Ana <anacaroldolata@gmail.com>

---
+++ src/views/book-read/book-read.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { SafeAreaView, View, StyleSheet, Text } from 'react-native'
+import DefaultBar from '../../components/default-bar'
+import safeView from '../../styles/safe-view'
+import BookReadEntries from './book-read-entries'
+
+const BookToRead = ({ navigation }) => {
+  return (
+    <SafeAreaView style={safeView.AndroidSafeArea}>
+      <View style={styles.container}>
+        <DefaultBar/>
+        <Text style={styles.title}>Livros que Eu Li</Text>
+        <BookReadEntries mocks={mocks} />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default BookToRead
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: '7%',
+    marginRight: '7%',
+    marginBottom: '5%'
+  },
+  title: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontSize: 22,
+    lineHeight: 33,
+    fontWeight: '500',
+    textAlign: 'left',
+    color: '#181D2D',
+    marginBottom: 22
+  }
+})
+
+let mocks = [{
+  'id': 1,
+  'book': 'Sankofa: A Novel',
+  'autor': 'Chibundu Onuzo',
+  'nota': 4,
+  'foto': '../../../assets/icons/Nickname.png'
+},
+{
+  'id': 2,
+  'book': 'O morro dos ventos uivantes',
+  'autor': 'Emily Bronte',
+  'nota': 3,
+  'foto': '../../../assets/icons/Nickname.png'
+},
+{
+  'id': 3,
+  'book': 'Admiravel mundo novo',
+  'autor': 'Aldous Huxley',
+  'nota': 3,
+  'foto': '../../../assets/icons/Nickname.png'
+},
+{
+  'id': 4,
+  'book': 'O morro dos ventos uivantes',
+  'autor': 'Emily Bronte',
+  'nota': 3,
+  'foto': '../../../assets/icons/Nickname.png'
+},
+]
+
+
+
+
