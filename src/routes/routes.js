a462e3770d066a9bf143a344631228888a970906
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2022-06-19 13:02:10 -0300

    add forget password route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const ROUTES = {
   signup: 'usuarios/',
   login: 'login/',
   profile: 'usuarios/visualizarPerfil/',
+  forget_password: '',
   edit_profile: 'usuarios/meusDados',
   update_profile: 'usuarios/atualizarDados',
   get_book: 'livros/',
