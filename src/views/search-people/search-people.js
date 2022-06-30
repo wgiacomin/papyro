0b9ad17fec9e355738b28d5f2589f96b426bc3eb
Wanderson <30360664+wgiacomin@users.noreply.github.com>
2022-06-29 21:20:39 -0300

    fix (#46)

--- src/views/search-people/search-people.js
+++ src/views/search-people/search-people.js
@@ -11,7 +11,7 @@ const SearchPeople = ({ navigation }) => {
   
   const [data, setData] = useState({
     page: 1,
-    loading: false,
+    loading: true,
   })
   
   const [people, setPeople] = useState([])
