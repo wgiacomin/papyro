f95090063006155235c715a5f35c9c0ae7e6db41
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2022-09-05 18:55:33 -0300

    add route

--- src/views/profile/use-profile.js
+++ src/views/profile/use-profile.js
@@ -3,8 +3,10 @@ import api from '../../routes/api'
 import { Alert } from 'react-native'
 import { BRANCH, PROFILE } from '@env'
 import CONTRACTS from '../../routes/contracts'
+import followButton from '../../../assets/buttons/followButton.png'
+import followingButton from '../../../assets/buttons/followingButton.png'
 
-function setValues({ setData, response, setProfile, self }) {
+function setValues({ setData, response, setProfile, self, setImage }) {
   if (self == null) {
     setProfile({
       id: response.data.id ? 0 : 1,
@@ -13,11 +15,17 @@ function setValues({ setData, response, setProfile, self }) {
       description: response.data.description,
       photo: response.data.photo
     })
+  } else {
+    if (response.data['you_follow']) {
+      setImage(followingButton)
+    } else {
+      setImage(followButton)
+    }
   }
   setData({ profile: response.data, loading: false })
 }
 
-async function useProfile({ setData, setProfile, id }) {
+async function useProfile({ setData, setProfile, id, setImage }) {
   if (BRANCH == 'dev' & PROFILE == 1) {
     setValues({ setProfile, response: CONTRACTS.profile.success })
     return
@@ -25,7 +33,7 @@ async function useProfile({ setData, setProfile, id }) {
 
   await api.get(ROUTES.view_profile, { params: { id } })
     .then((response) => {
-      setValues({ setData, setProfile, response, self: id })
+      setValues({ setData, setProfile, response, self: id, setImage })
     }).catch((error) => {
       Alert.alert('Atenção', error.response.data.detail)
       setData({ loading: false })
