525880ed7214a07f54ff7ee659b0baeada14555f
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2022-09-07 16:45:22 -0300

    fix

--- src/views/view-book/use-book.js
+++ src/views/view-book/use-book.js
@@ -3,6 +3,7 @@ import api from '../../routes/api'
 import { Alert } from 'react-native'
 import { BRANCH, GET_BOOK } from '@env'
 import CONTRACTS from '../../routes/contracts'
+import refresh_func from '../../routes/refresh'
 
 function setValues({ setBook, response }) {
   setBook({ book: response.data, loading: false })
@@ -13,14 +14,12 @@ async function useGetBook({ id, setBook, navigation }) {
     setValues({ setBook, response: CONTRACTS.get_book.success })
     return
   }
-
+  refresh_func()
   await api.get(ROUTES.get_book + id)
     .then((response) => {
       setValues({ setBook, response })
     }).catch((error) => {
-      navigation.goBack()
-      setBook({ loading: false })
-      Alert.alert('Atenção', error.response.data.detail)
+      useGetBook({ id, setBook, navigation })
     })
 
 }
